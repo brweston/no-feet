5610dfdc16d46f7226e3600e14dcb93bf00863e5
Bridget Weston <jablweston@gmail.com>
2018-11-12 19:18:07 -0800

    Don't run into self on first move

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 import bottle
 import os
 import random
+import numpy as np
 
 from api import *
 
+#Defining variables:
+#print(outputVector)
+#print(np.argmax(outputVector))
 
+#Routes:
 @bottle.route('/')
 def static():
     return "the server is running"
@@ -24,8 +29,6 @@ def start():
         bottle.request.urlparts.netloc
     )
 
-    # TODO: Do things with data
-
     print "Starting game %s" % data["game"]["id"]
     return StartResponse("#00ff00")
 
@@ -34,10 +37,42 @@ def start():
 def move():
     data = bottle.request.json
 
-    # TODO: Do things with data
-    
+    #Genome calculations:
+    a = np.matrix([[0.2, 0.5, 0.4, 0.3], [0.6, 0.9, 0.5, 0.2], [0.2, 0.1, 0.8, 0.7],
+               [0.3, 0.5, 0.2, 0.5], [0.2, 0.7, 0.4, 0.9], [0.3, 0.3, 0.7, 0.1],
+               [0.2, 0.5, 0.6, 0.9], [0.6, 0.2, 0.4, 0.6], [0.2, 0.6, 0.8, 0.2],
+               [0.6, 0.8, 0.6, 0.9]])
+
+    b = np.matrix([[random.random()], [random.random()], [random.random()], [random.random()], [random.random()],
+                   [random.random()], [random.random()], [random.random()], [random.random()], [random.random()]])
+    aTranspose = a.getT()
+    outputVector = aTranspose*b
+
+    #Get game board data
+    snakeHeadX = data["you"]["body"][0]["x"]
+    snakeHeadY = data["you"]["body"][0]["y"]
+    snakeBodyX = data["you"]["body"][1]["x"]
+    snakeBodyY = data["you"]["body"][1]["y"]
+
+    dX = snakeHeadX - snakeBodyX
+    dY = snakeHeadY - snakeBodyY
+
+    if (dX > 0):
+        #dont' move left
+        outputVector[2] *= 0
+    elif (dX < 0):
+        #don't move right
+        outputVector[3] *= 0
+    elif (dY > 0):
+        #don't move up
+        outputVector[0] *= 0
+    elif (dY < 0):
+        #don't move down
+        outputVector[1] *= 0
+
     directions = ['up', 'down', 'left', 'right']
-    direction = random.choice(directions)
+    direction = directions[np.argmax(outputVector)]
+    #direction = random.choice(directions)
 
     print "Moving %s" % direction
     return MoveResponse(direction)
@@ -46,8 +81,6 @@ def move():
 @bottle.post('/end')
 def end():
     data = bottle.request.json
-
-    # TODO: Do things with data
 
     print "Game %s ended" % data["game"]["id"]
 
